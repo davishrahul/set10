1ecb32c9720528dd0639810afb2da08ac3a6202b
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-07 10:12:29 +0530

    Update ques8.cpp

--- ques8.cpp
+++ ques8.cpp
@@ -3,17 +3,17 @@ using namespace std;
 int main()
 {
     int number;
-    cin>>number;
+    cin>>number;//4
     int arr[number];
     for(int i=0;i<number;i++)
     {
-        cin>>arr[i];
+        cin>>arr[i];// 1 10 3 4  
     }
-    for(int j=0;j<number;j++)
+    for(int j=0;j<number;j++) //0<4
     {
-        if(arr[j]>arr[j+1])
+        if(arr[j]!=j+1) //arr[2]!=3
         {
-            cout<<j;
+            cout<<j;//1
         }
     }
 }
